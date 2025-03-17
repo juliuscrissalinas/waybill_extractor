72052b87dd369b5655dd0a39a53462c8d96b99c8
julius.cris.salinas <julius.cris.salinas@gmail.com>
2025-03-17 21:03:02 +0800

    Add redirect from root URL to API endpoint

--- backend/waybill_project/urls.py
+++ backend/waybill_project/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("waybill.urls")),
+    path("", RedirectView.as_view(url="/api/", permanent=False)),
 ]
 
 # Serve media files in development
